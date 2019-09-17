aa379674072a4b93140785ce0ac8a60eafd5bc00
John R <johnrodriguezforero1@gmail.com>
2019-09-17 19:38:43 -0400

    Update ActionBuilder.class.php

--- controleur/ActionBuilder.class.php
+++ controleur/ActionBuilder.class.php
@@ -1,4 +1,5 @@
 <?php
+require_once('/controleur/AfficherRequestAction.class.php');
 require_once('/controleur/DefaultAction.class.php');
 require_once('/controleur/LoginAction.class.php');
 require_once('/controleur/LogoutAction.class.php');
@@ -12,7 +13,7 @@ class ActionBuilder{
 			case "deconnecter" :
 				return new LogoutAction();
 				break; 
-			case "afficher" :
+			case "afficherRequest" :
 				return new AfficherAction();
 				break; 
 			default :
