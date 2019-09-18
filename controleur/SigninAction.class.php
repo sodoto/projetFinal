8d71029008da7187832e74f82ab37da1915da742
John R <johnrodriguezforero1@gmail.com>
2019-09-17 21:29:11 -0400

    Update SigninAction.class.php

--- controleur/SigninAction.class.php
+++ controleur/SigninAction.class.php
@@ -1,48 +1,83 @@
 <?php
 require_once('./controleur/Action.interface.php');
-class LoginAction implements Action {
+require_once('./modele/InsertMemberDAO.class.php');
+class SigninAction implements Action {
 	public function execute(){
-		//esta instruccion hace que aparezca la pantalla de login en un principio
+		//esta instruccion permite cargar la pagina si no hay sesion todavia
 		if (!ISSET($_REQUEST["username"]))
-			return "login";
+			return "signin";
 		if (!$this->valide())
 		{
 			//$_REQUEST["global_message"] = "Le formulaire contient des erreurs. Veuillez les corriger.";	
-			return "login";
+			return "signin";
 		}
 		
-		require_once('/modele/membreDAO.class.php');
-		$mdao = new membreDAO();
-		$membre = $mdao->find($_REQUEST["username"]);
-		if ($membre == null)
-			{
-				$_REQUEST["field_messages"]["username"] = "Utilisateur inexistant.";	
-				//vuelve al login si el usuario no existe
-				return "login";
-			}
-		else if ($membre->getPassword() != $_REQUEST["password"])
-			{
-				$_REQUEST["field_messages"]["password"] = "Mot de passe incorrect.";	
-				//vuelve al usuario si el correo no es correcto
-				return "login";
-			}
-		if (!ISSET($_SESSION)) session_start();
+		$dao = new InsertMemberDAO();
+		
+		$member = new Member();
+		$member->setFirstname($_REQUEST['firstname']);
+		$member->setLastname($_REQUEST['lastname']);
+		$member->setCity($_REQUEST['city']);
+		$member->setEmail($_REQUEST['email']);
+		$member->setUsername($_REQUEST['username']);
+		$member->setPassword($_REQUEST['password']);
+		$dao->insertMember($member);
+		
+		
+		
+		require_once('/modele/MemberDAO.class.php');
+		$mdao = new MemberDAO();
+		$membre = $mdao->findUser($_REQUEST["username"]);
+		
+		if (!ISSET($_SESSION)) 
+		session_start();
 		$_SESSION["connected"] = $_REQUEST["username"];
 		$_SESSION["idMember"] = $membre->getIdMember();
 		//si esta todo correcto va a la vista afficher
-		return "afficherRequest";
+		return "afficherRequest";//debe seguir a los skills
 	}
+	//Verification de champs du formulaire
 	public function valide()
 	{
 		$result = true;
+		if ($_REQUEST['firstname'] == "")
+		{
+			$_REQUEST["field_messages"]["firstname"] = "Veuillez entrer votre prenom";
+			$result = false;
+		}	
+		if ($_REQUEST['lastname'] == "")
+		{
+			$_REQUEST["field_messages"]["lastname"] = "Veuillez entrer votre nom de famille";
+			$result = false;
+		}
+		if ($_REQUEST['city'] == "")
+		{
+			$_REQUEST["field_messages"]["city"] = "Veuillez entrer la ville";
+			$result = false;
+		}	
+		if ($_REQUEST['email'] == "")
+		{
+			$_REQUEST["field_messages"]["email"] = "Veuillez entrer le courrier électronique";
+			$result = false;
+		}	
 		if ($_REQUEST['username'] == "")
 		{
-			$_REQUEST["field_messages"]["username"] = "Donnez votre nom d'utilisateur";
+			$_REQUEST["field_messages"]["username"] = "Veuillez entrer votre nom d'utilisateur";
 			$result = false;
 		}	
 		if ($_REQUEST['password'] == "")
 		{
 			$_REQUEST["field_messages"]["password"] = "Mot de passe obligatoire";
+			$result = false;
+		}
+		if ($_REQUEST['password2'] == "")
+		{
+			$_REQUEST["field_messages"]["password2"] = "Retapez le mot de passe";
+			$result = false;
+		}	
+		if ($_REQUEST['password2'] != $_REQUEST['password'])
+		{
+			$_REQUEST["field_messages"]["passwordMissMatch"] = "Les deux mots de passe ne correspondent pas, veuillez vérifier";
 			$result = false;
 		}	
 		return $result;
