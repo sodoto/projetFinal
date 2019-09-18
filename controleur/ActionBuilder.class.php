9c841eaf20633ff16dfc20108138f5c8d8c13186
John R <johnrodriguezforero1@gmail.com>
2019-09-17 21:29:04 -0400

    Update ActionBuilder.class.php

--- controleur/ActionBuilder.class.php
+++ controleur/ActionBuilder.class.php
@@ -3,12 +3,17 @@ require_once('/controleur/AfficherRequestAction.class.php');
 require_once('/controleur/DefaultAction.class.php');
 require_once('/controleur/LoginAction.class.php');
 require_once('/controleur/LogoutAction.class.php');
+require_once('/controleur/SigninAction.class.php');
+
 class ActionBuilder{
 	public static function getAction($nomAction){
 		switch ($nomAction)
 		{
 			case "connecter" :
 				return new LoginAction();
+				break; 
+			case "signin" :
+				return new SigninAction();
 				break; 
 			case "deconnecter" :
 				return new LogoutAction();
