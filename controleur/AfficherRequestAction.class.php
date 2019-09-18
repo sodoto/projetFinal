917e2bc8be037f912f44284dc78b813c521b1edc
John R <johnrodriguezforero1@gmail.com>
2019-09-18 19:43:37 -0400

    Update AfficherRequestAction.class.php

--- controleur/AfficherRequestAction.class.php
+++ controleur/AfficherRequestAction.class.php
@@ -4,9 +4,9 @@ class AfficherRequestAction implements Action {
 	public function execute(){
 		if (!ISSET($_SESSION)) session_start();
 		
-		if (!ISSET($_SESSION["connected"])){  //utilisateur non connected.
-			return "login";  //si hay login v a mostrar de lo contrario va a indice
-		}
+		//if (!ISSET($_SESSION["connected"])){  //ici on peut bloquer que quelqu'un qui n'est pas enregistré puisse voir les requêtes
+		//	return "login";  
+		//}
 		else{
 			return "afficherRequest";
 		}
