f1550b582daa23492f25552f72db2497a8a45768
John R <johnrodriguezforero1@gmail.com>
2019-09-18 12:44:30 -0400

    Update MemberSkillsAction.class.php

--- controleur/MemberSkillsAction.class.php
+++ controleur/MemberSkillsAction.class.php
@@ -1,98 +1,101 @@
 <?php
 require_once('./controleur/Action.interface.php');
-require_once('/classes/Memberskills.class.php');
+require_once('/modele/classes/MemberSkills.class.php');
+require_once('./modele/MemberSkillsDAO.class.php');
 
 class MemberSkillsAction implements Action {
 	public function execute(){
-		if (!ISSET($_REQUEST["username"]))
-			return "signin";
+
+if (!ISSET($_SESSION))
+	session_start();
 	
-$idMember=$_SESSION["idMember"];
+$idM=$_SESSION["idMember"];
+
+$dao = new MemberSkillsDAO();
 	
 if (isset($_POST['submit'])) {
 	if(isset($_POST['deObj'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(1);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['peintA'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(2);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['peintO'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(3);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['trad'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(4);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['nett'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(5);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['transp'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(6);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['acomp'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(7);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['ense'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(8);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['org'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(9);
 		$dao->insertMemberSkills($memberskill);
 	}if(isset($_POST['cuis'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(10);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['nour'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(11);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['cond'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(12);
 		$dao->insertMemberSkills($memberskill);
 	}
 	if(isset($_POST['rep'])){
 		$memberskill = new Memberskills();
-		$memberskill->setIdMember($idMember);
+		$memberskill->setIdMember($idM);
 		$memberskill->setIdSkill(13);
 		$dao->insertMemberSkills($memberskill);
 	}
+	
 	return "afficherRequest";
 }
 
-if (isset($_POST['reset'])) {
-header('location:competences.php');//changer buttons off
-}	
 		
+}
 }
 ?>
