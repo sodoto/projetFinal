de98cb34c22810a8b244b8f4417b404a3422e243
John R <johnrodriguezforero1@gmail.com>
2019-09-19 10:01:01 -0400

    Create OfferRequestAction.class.php

---
+++ controleur/OfferRequestAction.class.php
@@ -0,0 +1,16 @@
+<?php
+require_once('./controleur/Action.interface.php');
+class OfferRequestAction implements Action {
+	public function execute(){
+		if (!ISSET($_SESSION)) session_start();
+		
+		if (!ISSET($_SESSION["connected"])){  
+		return "login";  
+		}
+		else{
+			return "offerRequest";
+		}
+					
+		
+	}
+}
