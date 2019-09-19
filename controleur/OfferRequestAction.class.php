d47aaed9c4cbf65b41e5548371352fba0c35b115
John R <johnrodriguezforero1@gmail.com>
2019-09-19 11:11:35 -0400

    Update OfferRequestAction.class.php

--- controleur/OfferRequestAction.class.php
+++ controleur/OfferRequestAction.class.php
@@ -1,5 +1,7 @@
 <?php
 require_once('./controleur/Action.interface.php');
+require_once('/classes/OfferRequestDAO.class.php');
+
 class OfferRequestAction implements Action {
 	public function execute(){
 		if (!ISSET($_SESSION)) session_start();
@@ -8,6 +10,17 @@ class OfferRequestAction implements Action {
 		return "login";  
 		}
 		else{
+			
+			$dao = new OfferRequestDAO();
+		
+			$offerRequest = new OfferRequest();
+			$offerRequest->setStatus($_REQUEST['status']);
+			$offerRequest->setDateOffer(date("Y/m/d"));
+			$offerRequest->setComment("Text");
+			$offerRequest->setIdMember($_REQUEST['idMember']);
+			$offerRequest->setIdRequest($_REQUEST['idRequest']);
+			$dao->create($offerRequest);
+			
 			return "offerRequest";
 		}
 					
