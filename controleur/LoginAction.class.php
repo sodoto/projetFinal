4c8d768a7d7016fe15f95ab651a0bf4a6bef3214
JohnRodriguezF <54953564+JohnRodriguezF@users.noreply.github.com>
2019-09-23 12:47:50 -0400

    Update LoginAction.class.php

--- controleur/LoginAction.class.php
+++ controleur/LoginAction.class.php
@@ -1,9 +1,12 @@
 <?php
 require_once('./controleur/Action.interface.php');
-if (!ISSET($_SESSION)) session_start();
 
 class LoginAction implements Action {
 	public function execute(){
+		if (!ISSET($_SESSION))
+		{
+			session_start();
+		}
 		if(ISSET($_SESSION["connected"])){
 			return "default";
 			echo $_SESSION["connected"];
@@ -32,7 +35,8 @@ class LoginAction implements Action {
 			}
 
 		$_SESSION["connected"] = $_REQUEST["email"];
-		$_SESSION["id"] = $member->getIdMember();
+		$_SESSION["idMember"] = $member->getIdMember();
+		$_SESSION["erreurRequest"] = false;
 		
 		return "afficherRequest";
 	}
