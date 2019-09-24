09a8aae0b7f488a11040dd913342eb6fd59e862c
JohnRodriguezF <54953564+JohnRodriguezF@users.noreply.github.com>
2019-09-24 15:45:06 -0400

    Update OfferRequestAction.class.php

--- controleur/OfferRequestAction.class.php
+++ controleur/OfferRequestAction.class.php
@@ -6,27 +6,17 @@ require_once('/modele/OfferRequestDAO.class.php');
 class OfferRequestAction implements Action {
 	public function execute(){
 		
-		if (!ISSET($_SESSION)) session_start();
+		if (!ISSET($_SESSION)) {
+			session_start();
+			
+		}
 		
 		if (!ISSET($_SESSION["connected"])){  
 		return "login";  
-		}
-		else{
-			
-				$dao = new OfferRequestDAO();
-				date_default_timezone_set("America/Toronto");
-			
-				$date = date('Y-m-d H:i:s');
-			
-				$offerRequest = new OfferRequest();
-				$offerRequest->setStatus($_SESSION["status"]);
-				$offerRequest->setDateOffer($date);
-				$offerRequest->setComment("Awaiting");
-				$offerRequest->setIdMember($_SESSION["idMember"]);
-				$offerRequest->setIdRequest($_SESSION["IDRequest"]);
-				$dao->create($offerRequest);
-		
-			return "offerRequest";
+		}else{
+
+				return "offerRequest";
+				
 		}
 					
 		
