de8bd5f8f8252aa57506e69899b10227655bf631
Samuel <samuel.theroux@gmail.com>
2019-09-24 16:32:48 -0400

    test javacript

--- vues/myOfferRequest.php
+++ vues/myOfferRequest.php
@@ -10,10 +10,11 @@
 <head>
 		<meta http-equiv="Content-Language" content="en-ca">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-		<link rel="stylesheet" href="./css/style.css" type="text/css" />
-		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<link type="text/css" rel="stylesheet" href="./css/style.css" type="text/css" />
+		<link type="text/css" rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+		<link type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
+		<script type="text/javascript" src="./javascript/javascript.js"></script>
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -21,6 +22,38 @@
 </head>
 
 <body>
+	<div id="myNav" class="overlay">
+		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+		<?php
+			require_once('/modele/RequestDAO.class.php');
+		?>
+		<div class="overlay-content">
+			<table>
+				<thead>
+					<tr>
+						<td>SKILL WANTED</td>
+						<td>DESCRIPTION</td>
+						<td>DATE REQUEST</td>
+						<td>DATE OF SERVICE</td>
+						<td>LOCATION</td>
+						<td>STATUS</td>
+						<td>ACTION</td>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+						<td></td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+	</div>
 	<div class="d-flex flex-column align-content-stretch bd-highlight" style="height: 100vh;">
 		<?php
 			include("banner.php");
@@ -56,7 +89,7 @@
                                 $request = $daoRequest->find($offerRequest->getIdRequest());
 					?>
 					<tr>
-						<td><?=$request->getTitle()?></td>
+						<td><a href="#" onclick="openNav()" title="Voir la demande"><?=$request->getTitle()?></a></td>
 						<td><?=date_format($dateOffer, "d/m/Y")?></td>
 						<td><?=$offerRequest->getComment()?></td>
 						<td><?=$offerRequest->getStatus()?></td>
