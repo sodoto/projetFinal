3df9457857a3f3ab27d6c32b4eab539c053c4c5d
Samuel <samuel.theroux@gmail.com>
2019-09-30 12:56:17 -0400

    frontend

--- vues/myRequest.php
+++ vues/myRequest.php
@@ -13,7 +13,6 @@
 		<link rel="stylesheet" href="./css/style.css" type="text/css" />
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> -->
 		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
@@ -27,7 +26,7 @@
 			//include("menu.php");
 		?>
 
-		<div>
+		<div class="d-flex flex-column align-items-center justify-content-center bd-highlight">
 			<h2>MES DEMANDES</h2>
 			<?php
 				require_once('/modele/RequestDAO.class.php');
@@ -46,90 +45,59 @@
 				}
 			?>
 
-			<div class="container">
-				<?php
-					require_once('/modele/SkillsDAO.class.php');
-					$daoSkills = new SkillsDAO();
-					$tRequest = $daoRequest->findByIdMember($_SESSION["idMember"]);
-					date_default_timezone_set('America/Toronto');
-					if(empty($tRequest)){
-						echo "Vous n'avez aucune demande!";
-					}
-					else{
-						foreach($tRequest as $request) {
-							$dateRequest = date_create($request->getDateRequest());
-							$dateService = date_create($request->getDateService());
-							$skill = $daoSkills->find($request->getSkillWanted());
-				?>
-				<div class="card">
-					<div class="card-header"><?=$request->getTitle()?></div>
-					<div class="card-body">
-						Date du service: <?=date_format($dateService, "d/m/Y")?> <br/>
-						Date de la demande: <?=date_format($dateRequest, "d/m/Y")?> <br/>
-						Location: <?=$request->getLocation()?> <br/>
-						Status: <?=$request->getStatus()?> <br/>
-						Habileté demandée: <?=$skill->getDescription()?> <br/>
-					</div>
-					<div class="card-footer">
-						<a href='?action=editRequest&idRequest=<?=$request->getIdRequest()?>' title='&eacute;diter'><i class="far fa-edit"></i></a>
-						<a href='?action=suppRequest&idRequest=<?=$request->getIdRequest()?>' title='effacer'><i class="far fa-trash-alt"></i></a>
-					</div>
-				</div>
-				<?php 
-                            }
-					    }
-					?>
-			</div>
-			<?php /*
-			<table class="table">
-				<thead class="thead-light">
-					<tr>
-						<td>SKILL WANTED</td>
-						<td>DESCRIPTION</td>
-						<td>DATE REQUEST</td>
-						<td>DATE OF SERVICE</td>
-						<td>LOCATION</td>
-						<td>STATUS</td>
-						<td>ACTION</td>
-					</tr>
-				</thead>
-				<tbody>
+				<div class="accordion" id="accordionRequest" role="tablist">
 					<?php
 						require_once('/modele/SkillsDAO.class.php');
 						$daoSkills = new SkillsDAO();
 						$tRequest = $daoRequest->findByIdMember($_SESSION["idMember"]);
+						$ariaExpanded = "true";
+						$collapsedShow = "show";
+						$collapsed = "";
 						date_default_timezone_set('America/Toronto');
-                        if(empty($tRequest)){
-                            echo "Vous n'avez aucune demande!";
-                        }
-                        else{
-						    foreach($tRequest as $request) {
+						if(empty($tRequest)){
+							echo "Vous n'avez aucune demande!";
+						}
+						else{
+							foreach($tRequest as $request) {
 								$dateRequest = date_create($request->getDateRequest());
 								$dateService = date_create($request->getDateService());
 								$skill = $daoSkills->find($request->getSkillWanted());
 					?>
-					<tr>
-						<td><?=$skill->getDescription()?></td>
-						<td><?=$request->getTitle()?></td>
-						<td><?=date_format($dateRequest, "d/m/Y")?></td>
-						<td><?=date_format($dateService, "d/m/Y")?></td>
-						<td><?=$request->getLocation()?></td>
-						<td><?=$request->getStatus()?></td>
-						<td>
-							<a href='?action=editRequest&idRequest=<?=$request->getIdRequest()?>' title='&eacute;diter'><i class="far fa-edit"></i></a>
-							<a href='?action=suppRequest&idRequest=<?=$request->getIdRequest()?>' title='effacer'><i class="far fa-trash-alt"></i></a>
-							<!--Para que no aparezca el item de adicionar se puede hacer un campo hiden CAMBIAR PARA USAR CAMPO HIDEN-->
-						</td>
-					</tr>
-                    <?php 
-                            }
-					    }
+					<div class="card mb-r">
+							<!-- <div class="" > -->
+							<h4 class="card-header" role="tab" id="heading<?=$request->getIdRequest()?>">
+								<a class="<?=$collapsed?> d-block" data-toggle="collapse" href="#collapse<?=$request->getIdRequest()?>"  aria-expanded="<?=$ariaExpanded?>" aria-controls="collapse<?=$request->getIdRequest()?>">
+									<span>
+									<i class="fa fa-chevron-down float-right"></i><?=$request->getTitle()?>
+									</span>
+								</a>
+							</h4>
+							<!-- </div> -->
+					
+						<div id="collapse<?=$request->getIdRequest()?>" class="collapse <?=$collapsedShow?>" aria-labelledby="heading<?=$request->getIdRequest()?>" data-parent="#accordionRequest">
+							<div class="card-body">
+								Date du service: <?=date_format($dateService, "d/m/Y")?> <br/>
+								Date de la demande: <?=date_format($dateRequest, "d/m/Y")?> <br/>
+								Location: <?=$request->getLocation()?> <br/>
+								Status: <?=$request->getStatus()?> <br/>
+								Habileté demandée: <?=$skill->getDescription()?> <br/>
+							</div>
+							<div class="card-footer">
+								<a href='?action=editRequest&idRequest=<?=$request->getIdRequest()?>' title='&eacute;diter'><i class="far fa-edit"></i></a>
+								<a href='?action=suppRequest&idRequest=<?=$request->getIdRequest()?>' title='effacer'><i class="far fa-trash-alt"></i></a>
+							</div>
+						</div>
+					</div>
+					<?php
+									$ariaExpanded = "false";
+									$collapsedShow = "";
+									$collapsed = "collapsed";
+								}
+							}
 					?>
-				</tbody>
-			</table>
-			*/ ?>
+				</div>
 			<form action="" method="POST">
-			<input name="action" value="newRequest" type="hidden" />
+				<input name="action" value="newRequest" type="hidden" />
 				<button type="submit">Nouvelle demande</button>
 			</form>
 		</div>
