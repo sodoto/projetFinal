832a5b5a9e957e0f28b189df4d311790cd19ee2f
JohnRodriguezF <54953564+JohnRodriguezF@users.noreply.github.com>
2019-10-01 12:40:44 -0400

    Update AfficherConversationsAction.class.php

--- controleur/AfficherConversationsAction.class.php
+++ controleur/AfficherConversationsAction.class.php
@@ -13,10 +13,47 @@ class AfficherConversationsAction implements Action {
 		
 		if (!ISSET($_SESSION["connected"])){  
 		return "login";  
-		}else{
+		}else{ 
+			
+					if(isset($_REQUEST['message'])) {
+					
+						if(isset($_REQUEST['IdMessage']))
+						{ 		 
+						$_SESSION["IdMessage"]= $_REQUEST['IdMessage']; 
+						}			
+						
+						
+						$idMember="";
+						$idMember=isset($_SESSION['idMember'])?$_SESSION['idMember']:'';
+						$IDRequest="";
+						$IDRequest=isset($_SESSION['IDRequest'])?$_SESSION['IDRequest']:'';
+						$idRecepteur="";
+						$idRecepteur=isset($_SESSION['idRecepteur'])?$_SESSION['idRecepteur']:'';
+						
+						$dao = new MessageDAO();
+						$dao->updateMessageLu(($_SESSION["IdMessage"]));//marcar mensaje como no leido
+						
+						
+						$daoMe = new MessageDAO();			
+						date_default_timezone_set("America/Toronto");
+					
+						$date = date('Y-m-d H:i:s');
 
-				return "afficherConversations";
+						$message=new Message();
+						$message->setMessage($_REQUEST['message']);
+						$message->setIdRequest($_SESSION["IdRequest"]);
+						$message->setIdMember($idMember);
+						$message->setIdRecepteur($_SESSION["idRecepteur"]);
+						$message->setDateHeure($date);
+						$message->setMessageLu("No");
+						
+						$daoMe->insert($message); //Enviar mensaje
 				
+
+						
+			}	
+			
+				return "afficherConversations";	
 		}
 					
 		
